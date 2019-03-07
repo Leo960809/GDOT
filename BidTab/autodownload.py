7c072339e6978f7939a2b79e9f65ff92e2c72cdb
Jinhao Jiang <jjiang323@gatech.edu>
2019-03-07 15:06:28 -0500

    Update

--- BidTab/autodownload.py
+++ BidTab/autodownload.py
@@ -1,6 +1,8 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+import webbrowser
+import csv
 
 
 def auto_download(letting_id):
@@ -13,11 +15,16 @@ def auto_download(letting_id):
 
 	soup = BeautifulSoup(src, "html.parser")
 
+	links = []
 	for link in soup.find_all("a", text="Tabulations of Bids"):
-		print(link.get("href"))
-
-
+		webbrowser.open("https://www.bidx.com" + link.get("href"), new=0, autoraise=True)
 
 if __name__ == "__main__":
 
-	auto_download("18121401")
+	with open("letting_id (2011.1-2019.1).csv", "r") as csvfile:
+		csv_reader = csv.reader(csvfile, delimiter="\\")
+		id_count = 0
+		for row in csv_reader:
+			auto_download(f'Letting ID: {", ".join(row)}'.split()[2])
+			id_count += 1
+		print(f"Processed {id_count} letting ids.")
